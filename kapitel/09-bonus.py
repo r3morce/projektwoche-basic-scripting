2cd4564c559e5a22ab891636916df92a507878a4
Mathias <mathias.schmidt@camparound.com>
2022-08-26 16:37:54 +0200

    Adjust bonus

--- kapitel/09-bonus.py
+++ kapitel/09-bonus.py
@@ -10,9 +10,10 @@
 # TODO:
 # 1. Schritt: Lest zwei Werte über die Konsole ein. Speichert diese als Integer in je einer Variablen (wert_1 und wert_2). Beachtet: Ihr dürft im Moment davon ausgehen, dass der Benutzer keine falschen Eingaben macht! (Teste mit print(wert1, wert2).)
 
-# 2. Schritt: Addiert die beiden Werte und speichert das Ergebnis in einer weiteren Variablen (ergebnis_add). Erweitert die 'print'-Zeile folgendermaßen: print('Ergebnis Addition von %d + %d = %d' % (wert1, wert2, ergebnis_add))
+# 2. Schritt: Addiert die beiden Werte und speichert das Ergebnis in einer weiteren Variablen (ergebnis_add). Erweitert die 'print'-Zeile folgendermaßen: print('Ergebnis Addition von %d + %d = %d' % (wert_1, wert_2, ergebnis_add))
 
 # 3. Schritt: Fügt drei weitere Variablen (ergebnis_sub, ergebnis_mul und ergebnis_div) hinzu. Speichert das Ergebnis der Subtraktion (-), das Ergebnis der Multiplikation (*) und das Ergebnis der Division (/) in der jeweiligen Variablen. Fügt nun auch die passenden print-Befehle in das Programm ein.
 
 # 4. Schritt: Extra-Challenge: Das Ergebnis der Division stimmt leider nicht, wenn keine natürliche Zahl rauskommt. Welchen Datentyp braucht ihr für Kommazahlen? 
+
 # Beachtet: Auch beim print-Befehl müsst ihr Anpassungen vornehmen.
