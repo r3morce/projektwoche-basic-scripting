b01083395e4e7e397b2ceefb6ae0bf00ee8567be
Mathias <mathias.schmidt@hey.com>
2022-08-26 20:43:56 +0200

    Refine readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # projektwoche-basic-scripting
 Eine Aufgabensammlung um die Grundlage in der Script-Sprache Python zu lernen
+
+# Wie fange ich an?
+
+- Klicke auf den grünen Button oben rechts auf dem "Code" steht 
+- Wähle "Download ZIP" an und merke dir wo du die ZIP Datei speicherst
+- Navigiere in deinem Explorer zu der ZIP Datei und entpacke sie
+- Öffne die Seite https://www.online-python.com/ in deinem Browser
+- Klicke oben rechts auf die Sonne um den Dark Mode zu aktivieren
+- Wähle oben rechts die Einstellungen an
+    - Klicke oben rechts neben "Theme" und wähle "Monokai" aus 
+    - Aktiviere "Indented Soft Wrap" indem du auf das weiße Kästchen klickst
+- Wähle oben links das Icon mit dem Ordner an (Open File from Disc)
+- Navigiere in den Ordner in den du die ZIP Datei entpackt hast
+- Navigieren weiter in den Ordner "kapitel"
+- Öffne dort die Datei `01-hello-world.py`
+- In deinem Online Python Editor sollte sich nun die Datei mit allen weiteren Infos öffnen
--- kapitel/01-hello-world.py
+++ kapitel/01-hello-world.py
@@ -1,11 +1,11 @@
-# Mit dem Befehl "print" können Buchstaben oder Zahlen in der Konsole ausgegeben werden.
+# Mit dem Befehl "print" werden Buchstaben oder Zahlen in der Konsole ausgegeben.
 # Was ausgegeben werden soll, muss in () hinter dem Befehl stehen.
-# Wenn Buchstaben ausgegeben werden sollen bzw. eine Zeichenkette, müssen diese in 'einfache' oder "doppelte" Anführungszeichen gesetzt werden. Ich bevorzuge "doppelte".
-# Wenn Zahlen ausgegeben werden sollen, können diese ohne Anführungszeichen hingeschrieben werden
+# Sollen Buchstaben bzw. Zeichenketten ausgegeben werden, müssen diese in 'einfache' oder "doppelte" Anführungszeichen gesetzt werden.
+# Sollen Zahlen ausgegeben werden, können diese ohne Anführungszeichen hingeschrieben werden
 
 # Ein Beispiel
 print("Hallo Welt!")
 
-# TODO: Klick auf “Run” und schau was in der Konsole passiert. 
+# TODO: Klick auf “Run” und schau was in der Konsole passiert.
 
 # TODO: Passt den Code so an, dass er Hallo Vorname! ausgibt (z.B. Hallo Christian!).
