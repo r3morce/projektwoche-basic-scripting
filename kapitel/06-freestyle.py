b5e1727c94e6665f2abc19ccd4394616e5498168
Mathias <mathias.schmidt@camparound.com>
2022-08-26 14:51:34 +0200

    Add kapitel 6

---
+++ kapitel/06-freestyle.py
@@ -0,0 +1,9 @@
+# Freestyle: Lernt die Ausgabe (das, was unten in der Konsole angezeigt wird, nachdem ihr auf "Run" geklickt habt) besser kennen.
+
+# Was passiert, wenn ihr die Zahlen über die Konsole einlest, statt einen festen Wert zu vergeben?
+# Tipp: Werte könnt ihr mit input("...") einlesen. Bei der Aufgabe mit Name und Alter hatten wir das schon.
+
+zahl_1 = 3
+zahl_2 = 3
+
+print(zahl_1 + zahl_2)
