8a945ce7b2c9a4fe4d5c9da8b4a1cd7bc77416b8
Mathias <mathias.schmidt@camparound.com>
2022-08-26 14:47:52 +0200

    Add kapitel 5

---
+++ kapitel/05-freestyle.py
@@ -0,0 +1,12 @@
+# Freestyle: Lernt die Ausgabe (das, was unten in der Konsole angezeigt wird, nachdem ihr auf
+# "Run" geklickt habt) besser kennen.
+
+# Hier wird der Variablen "name" eine Eingabe (Input) aus der Konsole zugewiesen
+nickname = input("Wie ist dein Nickname?")
+
+# Hier wirde der Text "Hallo" und der abgefragte nickname ausgegeben.
+# Über das Komma werden die gewünschten Elemente für die Ausgabe aufgelistet.
+print("Hallo", nickname, "!")
+
+# TODO: Was passiert, wenn ihr 'Hallo', nickname und '!' in drei verschiedenen print Anweisungen
+# ausgeben lasst, statt in einer?
